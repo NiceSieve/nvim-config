f664291b5ea13419d07897de9259452708e156af
Nice Sieve <92229915+NiceSieve@users.noreply.github.com>
2022-07-12 00:02:20 +0300

    add ? completion

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -89,8 +89,14 @@ cmp.setup({
 })
 
 
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+cmp.setup.cmdline('?', {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
 		{ name = 'buffer' }
