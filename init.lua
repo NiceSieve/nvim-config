bfffbaa86e93a619b654dcdc7a00614074a90a73
Nice Sieve <92229915+NiceSieve@users.noreply.github.com>
2022-07-12 11:24:29 +0300

    add notimeout and commend init.lua

--- init.lua
+++ init.lua
@@ -8,20 +8,21 @@ o.hidden = true --don't close buffers when opening another
 o.list = true --show whitespace
 o.tabstop = 4 --tab size
 o.shiftwidth = 0 --follow tabstop
-o.wrap = false
+o.wrap = false -- nice for text, but makes codn unreadable
 
 -- terminal bettering settings
 o.mouse = "a"
 o.termguicolors = true
 
 -- search configs
-o.incsearch = true
-o.ignorecase = true
-o.wildignorecase = true
+o.incsearch = true  -- incremental search
+o.ignorecase = true -- case insensitive
+o.wildignorecase = true -- case insensitive completion too
 
-vim.g.mapleader = ' '
-vim.keymap.set('t', '<C-\\>', '<C-\\><C-n>')
-vim.keymap.set('n', '<Esc>', function() vim.fn.setreg("/", nil) end)
+vim.g.mapleader = ' ' -- space is the leader cuz it's the biggest
+o.timeout = false -- I'm slow
+vim.keymap.set('t', '<C-\\>', '<C-\\><C-n>') -- exit term mode easier
+vim.keymap.set('n', '<Esc>', function() vim.fn.setreg("/", nil) end) -- clear search results
 
 -- Find files using Telescope command-line sugar.
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
