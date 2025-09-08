491bb506d040f28748390cf608e48ad1c7b31a1c
NiceSieve <92229915+NiceSieve@users.noreply.github.com>
2025-09-08 21:40:53 +0300

    update nvim-treesitter to main branch

--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -3,7 +3,7 @@ return {
 		"Mofiqul/dracula.nvim"
 	},
 	{ -- fuzzy menu
-		'nvim-telescope/telescope.nvim', branch = '0.1.x',
+		'nvim-telescope/telescope.nvim',
 		dependencies = {
 			{'nvim-lua/plenary.nvim'},
 			{'nvim-telescope/telescope-fzf-native.nvim', build='make'}
