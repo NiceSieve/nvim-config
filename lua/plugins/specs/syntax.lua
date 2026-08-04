5c5a09e58fa099ee28b73128ba6fa8ccb8cf43dc
NiceSieve <92229915+NiceSieve@users.noreply.github.com>
2026-08-04 23:05:19 +0300

    optimize treesitter plugin startup

--- lua/plugins/specs/syntax.lua
+++ lua/plugins/specs/syntax.lua
@@ -4,35 +4,54 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		client.server_capabilities.semanticTokensProvider = nil
 	end,
 });
-return {
-	'nvim-treesitter/nvim-treesitter',
-	build=':TSUpdate',
-	branch='main',
-	dependencies={
-		{
-			'romgrk/nvim-treesitter-context', 
-			opts = { -- show context at top
-				enable=true,
-				multiwindow=true,
-				mode='topline',
-			},
-			init = function()
-				vim.cmd "hi TreesitterContextBottom gui=underline guisp=Purple"
-				vim.cmd "hi TreesitterContextLineNumberBottom gui=underline guisp=Purple"
-			end,
-		},
-		{'nvim-treesitter/nvim-treesitter-textobjects',
-			branch='main'
-		},
-		{'RRethy/nvim-treesitter-endwise'}, -- autopairs for lua
-		{'JoosepAlviste/nvim-ts-context-commentstring', opts={enable_autocmd=false}},
+return {{
+		'nvim-treesitter/nvim-treesitter',
+		build=':TSUpdate',
+		branch='main',
+		config=function()
+			local get_option = vim.filetype.get_option
+			vim.filetype.get_option = function(filetype, option)
+				return option == "commentstring"
+				and require("ts_context_commentstring.internal").calculate_commentstring()
+				or get_option(filetype, option)
+			end
+		end 
 	},
-	config=function()
-		local get_option = vim.filetype.get_option
-		vim.filetype.get_option = function(filetype, option)
-			return option == "commentstring"
-			and require("ts_context_commentstring.internal").calculate_commentstring()
-			or get_option(filetype, option)
+	{
+		'romgrk/nvim-treesitter-context', 
+		opts = { -- show context at top
+			enable=true,
+			multiwindow=true,
+			mode='topline',
+		},
+		init = function()
+			vim.cmd "hi TreesitterContextBottom gui=underline guisp=Purple"
+			vim.cmd "hi TreesitterContextLineNumberBottom gui=underline guisp=Purple"
+		end,
+		event = "VeryLazy",
+	},
+	{
+		'nvim-treesitter/nvim-treesitter-textobjects',
+		branch='main',
+		event = "VeryLazy",
+	},
+	{ -- autopairs for lua
+		'RRethy/nvim-treesitter-endwise',
+		event = "VeryLazy",
+	},
+	{
+		'JoosepAlviste/nvim-ts-context-commentstring',
+		event = "VeryLazy",
+		config = function()
+			require('ts_context_commentstring').setup {
+				enable_autocmd = false,
+			}
+			local get_option = vim.filetype.get_option
+			vim.filetype.get_option = function(filetype, option)
+				return option == "commentstring"
+				and require("ts_context_commentstring.internal").calculate_commentstring()
+				or get_option(filetype, option)
+			end
 		end
-	end 
+	},
 }
