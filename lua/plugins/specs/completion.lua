a1110595d5fca919bdfe13c66faea54736cf49e2
Nice Sieve <92229915+NiceSieve@users.noreply.github.com>
2026-08-10 14:38:29 +0300

    tiny-cmdline attempt

--- lua/plugins/specs/completion.lua
+++ lua/plugins/specs/completion.lua
@@ -1,171 +1,61 @@
 return {
-	'hrsh7th/nvim-cmp',
-	dependencies={
-		{'hrsh7th/cmp-buffer'}, -- search in buffer(s)
-		{'hrsh7th/cmp-calc'}, -- do maths
-		{'hrsh7th/cmp-cmdline'}, -- complete commands
-		{'hrsh7th/cmp-nvim-lua'}, -- nvim lua API
-		{'hrsh7th/cmp-nvim-lsp', dependencies={'neovim/nvim-lspconfig'}}, -- LSP, when I do that
-		{'hrsh7th/cmp-path'}, -- complete paths
-		{'dmitmel/cmp-cmdline-history'}, -- command history
-		{'dmitmel/cmp-digraphs'}, -- special raku chars
-		{
-			'saadparwaiz1/cmp_luasnip',
-			dependencies= {
-				'L3MON4D3/LuaSnip', -- snippet engine
-				config = function()
-					require('luasnip').filetype_extend("all", { "_" })
-					require("luasnip.loaders.from_snipmate").lazy_load()
-				end,
-				build = "make install_jsregexp",
-				module = 'luasnip',
-				dependencies={'honza/vim-snippets'} -- snippet collection
-			},
-		}, -- snippet completion
-		{"nvim-tree/nvim-web-devicons"},
+	'saghen/blink.cmp',
+	-- optional: provides snippets for the snippet source
+	dependencies = {
+		'rafamadriz/friendly-snippets',
+		-- luasnip is bigger than I need, but I'll keep a note here
+		-- { 
+		-- 'L3MON4D3/LuaSnip', version = 'v2.*',
+		-- -- install jsregexp (optional!:).
+		-- build = "make install_jsregexp"
+		-- dependencies = {'honza/vim-snippets'},
+		-- config = function
+			-- require("luasnip.loaders.from_snipmate").lazy_load()
+		-- end
+		-- },
 	},
-	config = function() 
-		local cmp = require('cmp')
-		local luasnip = require('luasnip')
-
-		local kind_icons = {
-			Text = "",
-			Method = "󰆧",
-			Function = "󰊕",
-			Constructor = "",
-			Field = "󰇽",
-			Variable = "󰂡",
-			Class = "󰠱",
-			Interface = "",
-			Module = "",
-			Property = "󰜢",
-			Unit = "",
-			Value = "󰎠",
-			Enum = "",
-			Keyword = "󰌋",
-			Snippet = "",
-			Color = "󰏘",
-			File = "󰈙",
-			Reference = "",
-			Folder = "󰉋",
-			EnumMember = "",
-			Constant = "󰏿",
-			Struct = "",
-			Event = "",
-			Operator = "󰆕",
-			TypeParameter = "󰅲",
-		}
-		function has_words_before()
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
-
-		cmp.setup({
-			window = {
-				completion = cmp.config.window.bordered(),
+	opts = {
+		keymap = {
+			preset = 'enter',
+		},
+		completion = {
+			keyword = {
+				range = 'full',
 			},
-			snippet = {
-				-- REQUIRED - you must specify a snippet engine
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
+			ghost_text = {
+				enabled = true
 			},
-			mapping = {
-				['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-				['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-				['<C-n>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end,{ 'i', 'c' }),
-				['<C-p>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-0)
-					else
-						fallback()
-					end
-				end, { 'i', 'c' }),
-				['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-				['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-				['<C-e>'] = cmp.mapping({
-					i = cmp.mapping.abort(),
-					c = cmp.mapping.close(),
-				}),
-				-- Accept currently selected item. If none selected, `select` first item.
-				-- Set `select` to `false` to only confirm explicitly selected items.
-				['<CR>'] = cmp.mapping.confirm({ select = false }),
-			},
-			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' }, -- for LSP
-				{ name = 'luasnip' }, -- For luasnip users.
-				{ name = 'nvim_lua' }
-			},{
-				{ name = 'calc' },
-				{ name = 'path' },
-				{
-					name = 'buffer',
-					option = {
-						get_bufnrs = function()
-							return vim.api.nvim_list_bufs()
-						end
-					}
+			list = {
+				selection = {
+					preselect = false,
+					auto_insert = false,
 				},
-			},{
-				{ name = 'digraphs'}
-			}),
-			formatting = {
-				format = function(entry, vim_item)
-					-- Kind icons
-					vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
-					-- Source
-					vim_item.menu = ({
-						buffer = "[buff]",
-						nvim_lsp = "[LSP]",
-						luasnip = "[snip]",
-						nvim_lua = "[lua]",
-						latex_symbols = "[LaTeX]",
-						cmdline_history = "[hist]",
-						cmdline = "[cmd]",
-						calc = "[calc]",
-						path = "[path]",
-					})[entry.source.name]
-					return vim_item
-				end
+			},
+		},
+		cmdline = {
+			keymap = {
+				preset = 'inherit'
+			},
+			completion = {
+				ghost_text = { enabled = true },
+				menu = {
+					auto_show = true,
+				},
+				list = {
+					selection = {
+						preselect = false,
+						auto_insert = false,
+					},
+				},
+			},
+		},
+		sources = {
+			providers = {
+				-- show buffer completions even when LSP
+				lsp = { fallbacks = {} },
 			}
-		})
-
-		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-		cmp.setup.cmdline('/', {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = 'buffer' }
-			}
-		})
-		cmp.setup.cmdline('?', {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = 'buffer' }
-			}
-		})
-
-		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-		cmp.setup.cmdline(':', {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = 'cmdline_history' },
-				{ name = 'cmdline' }
-			},{
-				{ name = 'buffer' }
-			})
-		})
-	end,
-	event = {'InsertEnter *', 'CmdlineEnter *'}
+		},
+	},
+	version = '1.*',
 }
+
--- lua/plugins/specs/ui.lua
+++ lua/plugins/specs/ui.lua
@@ -78,31 +78,77 @@ return {
 		} 
 	},
 	{
-		"folke/noice.nvim",
+		"rachartier/tiny-cmdline.nvim",
 		event = "VeryLazy",
-		opts = {
-			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-				},
-			},
-			presets = {
-				command_palette = true,
-			},
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			{
-				"rcarriga/nvim-notify",
-				init = function() vim.cmd "highlight link NormalNC Normal" end
-			}
-		}
+		init = function()
+			vim.o.cmdheight = 0
+		end,
+		config = function()
+			require("vim._core.ui2").enable({})
+			require("tiny-cmdline").setup({
+				highlighter = true,
+				on_reposition = require("tiny-cmdline").adapters.blink,
+				native_types = {},
+				position = { y = "10%" },
+			})
+			-- Fetch the live colors table directly from the plugin module
+			local dracula_colors = require("dracula").colors()
+
+			-- Apply those plugin values directly to your custom highlight groups
+			vim.api.nvim_set_hl(0, "CmdlineCommand", { fg = dracula_colors.purple, bold = true }) 
+			vim.api.nvim_set_hl(0, "CmdlineSearch",  { fg = dracula_colors.yellow, bold = true }) 
+
+			local blink_cmdline_sync = vim.api.nvim_create_augroup("BlinkCmdlineColorSync", { clear = true })
+
+			vim.api.nvim_create_autocmd("CmdlineEnter", {
+				group = blink_cmdline_sync,
+				callback = function()
+					-- Safely acquire live values from the theme
+					local colors = require("dracula").colors()
+					local current_mode = vim.fn.getcmdtype()
+
+					if current_mode == ":" then
+						-- Re-link tiny-cmdline explicitly to the active theme palette
+						vim.api.nvim_set_hl(0, "TinyCmdlineBorder", { link = "CmdlineCommand" })
+						vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { link = "CmdlineCommand" })
+
+					elseif current_mode == "/" or current_mode == "?" then
+						-- Re-link search UI elements explicitly to the active theme palette
+						vim.api.nvim_set_hl(0, "TinyCmdlineBorder", { link = "CmdlineSearch" })
+						vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { link = "CmdlineSearch" })
+					end
+				end,
+			})
+
+			-- Revert border rules back to normal when exiting command mode
+			vim.api.nvim_create_autocmd("CmdlineLeave", {
+				group = blink_cmdline_sync,
+				callback = function()
+					vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { link = "FloatBorder" })
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "BlinkCmpMenuPositionUpdate",
+				callback = function()
+					if vim.fn.mode() ~= "c" then
+						return
+					end
+
+					local ok_ui2, ui2 = pcall(require, "vim._core.ui2")
+					local cmd_win = ok_ui2 and ui2.wins and ui2.wins.cmd
+					if not cmd_win or not vim.api.nvim_win_is_valid(cmd_win) then
+						return
+					end
+					local cmd_cfg = vim.api.nvim_win_get_config(cmd_win)
+
+					local ok_menu, menu = pcall(require, "blink.cmp.completion.windows.menu")
+					if ok_menu and menu.win and menu.win:is_open() then
+						pcall(function() menu.win:set_width(cmd_cfg.width) end)
+						pcall(function() menu.win:set_win_config({relative="editor", row=vim.g.ui_cmdline_pos[1], col=cmd_cfg.col}) end)
+					end
+				end,
+			})
+		end,
 	}
 }
