eb55705917d522f19ddd897142120a390363a483
Nice Sieve <92229915+NiceSieve@users.noreply.github.com>
2026-08-27 22:37:28 +0300

    use newer/simpler alternatives for some stuff

--- lua/plugins/specs/editor.lua
+++ lua/plugins/specs/editor.lua
@@ -23,7 +23,8 @@ return {
 		opts={win={border='single'}},
 	},
 	{ -- surround text with stuff
-		'machakann/vim-sandwich',
+		'nvim-mini/mini.surround',
+		config=true,
 		keys='s',
 	},
 	{ -- auto close parens etc.
@@ -48,8 +49,11 @@ return {
 		keys={{'<leader>u', '<cmd>UndotreeToggle<cr>', desc="Open Undo Tree"}},
 	},
 	{ -- turn lines into columns
-		'FooSoft/vim-argwrap',
-		keys={{'<leader>a', '<cmd>ArgWrap<cr>'}},
+		'Wansmer/treesj',
+		keys={{'<leader>a', '<cmd>TSJToggle<cr>'}},
+		opts={
+			use_default_keymaps = false,
+		}
 	},
 	{ -- better quickfix window
 		'kevinhwang91/nvim-bqf',
@@ -109,11 +113,34 @@ return {
 		opts={},
 	},
 	{ -- clipboard manager
-		"AckslD/nvim-neoclip.lua",
+		"gbprod/yanky.nvim",
 		event = "TextYankPost",
-		keys = {{'<leader>fy', '<cmd>Telescope neoclip default<cr>'}},
+		keys = {
+			{'<leader>fy', '<cmd>Telescope yank_history<cr>'},
+			{ "<leader>p", "<cmd>YankyRingHistory<cr>", mode = { "n", "x" }, desc = "Open Yank History" },
+			{ "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
+			{ "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
+			{ "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
+			{ "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor and leave cursor after" },
+			{ "gP", "<Plug>(YankyGPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor and leave cursor after" },
+			{ "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Select previous entry through yank history" },
+			{ "<c-n>", "<Plug>(YankyNextEntry)", desc = "Select next entry through yank history" },
+			{ "]p", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+			{ "[p", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+			{ "]P", "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
+			{ "[P", "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
+			{ ">p", "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and indent right" },
+			{ "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
+			{ ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
+			{ "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
+			{ "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
+			{ "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
+		},
 		opts={
-			enable_persistent_history = true,
+			ring = {
+				history_length = 1000,
+				storage = "sqlite",
+			}
 		},
 		dependencies={
 			{'kkharji/sqlite.lua', module = 'sqlite'},
